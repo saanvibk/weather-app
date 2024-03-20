5a2caefd1e177878ec8e3a360f4a6cba923c5f00
prajwalshetty05 <prajwalshetty592005@gmail.com>
2024-03-20 20:18:10 +0530

    updated files

--- script.js
+++ script.js
@@ -1,20 +1,30 @@
 function getWeather() {
+
     let location = document.getElementById("location").value
     console.log("getting weather for city : " + location);
     const data = fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=2d0abadf28f4c24129fb4b49b93514bc`);
     data.then(res => res.json()).then(data => {
-        document.getElementById("cityName").innerHTML = +data.name
+
+
+        document.getElementById("cityName").innerHTML = + data.name
         document.getElementById("humidity").innerHTML = "humidity " + data.main.humidity + "%";
-        document.getElementById("description").innerHTML = data.weather[1]
+        document.getElementById("description").innerHTML = data.weather.description
+        document.getElementById("icon").innerHTML = data.weather.icon
         document.getElementById("country").innerHTML = data.sys.country;
-        document.getElementById("temp").innerHTML = data.main.temp
-        document.getElementById("feelLike").innerHTML = data.main.feels_like
-        document.getElementById("maxTemp").innerHTML = data.main.temp_max
-        document.getElementById("minTemp").innerHTML = data.main.temp_min
+        document.getElementById("temp").innerHTML = data.main.temp + "<span>&#8451;</span>"
+        document.getElementById("feelLike").innerHTML = data.main.feels_like + "<span>&#8451;</span>"
+        document.getElementById("maxTemp").innerHTML = data.main.temp_max + "<span>&#8451;</span>"
+        document.getElementById("minTemp").innerHTML = data.main.temp_min + "<span>&#8451;</span>"
         document.getElementById("sunRise").innerHTML = data.sys.sunrise
         document.getElementById("sunSet").innerHTML = data.sys.sunset
         document.getElementById("wind").innerHTML = data.wind.speed
-        document.getElementById("deg").innerHTML = data.wind.deg
+        document.getElementById("deg").innerHTML = data.wind.deg + "&deg"
+
+
+
+        let icon = data.weather.icon;
+        let iconUrl = `https://openweathermap.org/img/wn/${icon}@2x.png`
+        document.getElementById("icon").innerHTML = `<img src="${iconUrl}" alt="Icon">`;
     }).catch(
         console.log("cannot fetch weather")
     );
