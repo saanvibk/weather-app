f3706310b7343f29fee15ea6826b01de9009494b
prajwalshetty05 <prajwalshetty592005@gmail.com>
2024-03-20 18:53:07 +0530

    updated files

--- script.js
+++ script.js
@@ -1,25 +1,20 @@
-// locaton = document.getElementById("location").value
-// cityName = document, getElementById("cityName").value
-// country=document.getElementById("country").value
-
-
 function getWeather() {
-
-
     let location = document.getElementById("location").value
     console.log("getting weather for city : " + location);
     const data = fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=2d0abadf28f4c24129fb4b49b93514bc`);
     data.then(res => res.json()).then(data => {
+        document.getElementById("cityName").innerHTML = +data.name
         document.getElementById("humidity").innerHTML = "humidity " + data.main.humidity + "%";
+        document.getElementById("description").innerHTML = data.weather[1]
         document.getElementById("country").innerHTML = data.sys.country;
         document.getElementById("temp").innerHTML = data.main.temp
         document.getElementById("feelLike").innerHTML = data.main.feels_like
-        document.getElementById("maxTemp").innerHTML = data.main.temp_min
-        document.getElementById("minTemp").innerHTML = data.main.temp_max
+        document.getElementById("maxTemp").innerHTML = data.main.temp_max
+        document.getElementById("minTemp").innerHTML = data.main.temp_min
         document.getElementById("sunRise").innerHTML = data.sys.sunrise
         document.getElementById("sunSet").innerHTML = data.sys.sunset
-        document.getElementById("sunRise").innerHTML = data.wind.speed
-        document.getElementById("sunSet").innerHTML = dtaa.wind.deg
+        document.getElementById("wind").innerHTML = data.wind.speed
+        document.getElementById("deg").innerHTML = data.wind.deg
     }).catch(
         console.log("cannot fetch weather")
     );
