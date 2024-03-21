298aa772012ab75df618d72d3a635d560b95a71c
prajwalshetty05 <prajwalshetty592005@gmail.com>
2024-03-21 11:51:42 +0530

    updated files

--- script.js
+++ script.js
@@ -9,19 +9,17 @@ function getWeather() {
         document.getElementById("city").innerHTML = + data.name
         document.getElementById("humidity").innerHTML = "humidity " + data.main.humidity + "%";
         document.getElementById("description").innerHTML = data.weather[0].description
-        document.getElementById("icon").innerHTML = data.weather[0].icon
         document.getElementById("country").innerHTML = data.sys.country;
         document.getElementById("temp").innerHTML = data.main.temp + "<span>&#8451;</span>"
         document.getElementById("feelLike").innerHTML = data.main.feels_like + "<span>&#8451;</span>"
         document.getElementById("maxTemp").innerHTML = data.main.temp_max + "<span>&#8451;</span>"
         document.getElementById("minTemp").innerHTML = data.main.temp_min + "<span>&#8451;</span>"
-
         document.getElementById("wind").innerHTML = data.wind.speed + "m/s"
         document.getElementById("deg").innerHTML = data.wind.deg + "&deg"
 
         //get sunrise time with Date
-
         let sunRise = data.sys.sunrise;
+        console.log(sunRise)
         const rise = new Date(sunRise * 1000)
         console.log("with time", date.toTimeString());
         document.getElementById("sunRise").innerHTML = rise
@@ -29,6 +27,7 @@ function getWeather() {
         //get sunset time with Date//
 
         let sunSet = data.sys.sunrise;
+        console.log(sunSet)
         const set = new Date(sunRise * 1000)
         document.getElementById("sunSet").innerHTML = set
 
